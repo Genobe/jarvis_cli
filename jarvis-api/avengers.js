e700610ecbb405898cbae60f7958a5b02e5978c7
root <root@onyx.localdomain>
2018-06-10 15:23:15 -0400

    fixed name issue

--- jarvis-api/avengers.js
+++ jarvis-api/avengers.js
@@ -14,7 +14,7 @@ class Avengers {
 
 
   assemble(cb) {
-    Promise.all([this.marvel.characters.findByName('Thor'),this.marvel.characters.findByName('Hulk'),this.marvel.characters.findByName('Vision'),this.marvel.characters.findByName('spider-name'),this.marvel.characters.findByName('Captain America')]).then(function(responses) {
+    Promise.all([this.marvel.characters.findByName('Thor'),this.marvel.characters.findByName('Hulk'),this.marvel.characters.findByName('Vision'),this.marvel.characters.findByName('spider-man'),this.marvel.characters.findByName('Captain America')]).then(function(responses) {
     
       let avengers = []
       responses.forEach(function(character) {
